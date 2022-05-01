2fd7eac188987c591dd64c3c8e198c42861f7384
Layne <1297625071@qq.com>
2022-05-01 15:43:20 +0800

    firstCommit

--- thymeleaf/src/main/java/com/example/thymeleaf/ThymeleafApplication.java
+++ thymeleaf/src/main/java/com/example/thymeleaf/ThymeleafApplication.java
@@ -8,6 +8,7 @@ public class ThymeleafApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(ThymeleafApplication.class, args);
+        System.out.println("hei");
     }
 
 }
